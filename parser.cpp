866af262639946bb37f69a9dd8b4dcb0227753ea
Shaunak Kishore <kshaunak@gmail.com>
2023-08-12 15:36:17 -0400

    Use std::string view instead of ours

--- parser.cpp
+++ parser.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -13,30 +14,6 @@ struct Diagnostic {
   size_t pos;
   std::string error;
 };
-
-// string_view, since we are targeting C++ 14.
-
-struct string_view {
-  string_view() : m_data(nullptr), m_size(0) {}
-  string_view(const char* data) : m_data(data), m_size(strlen(data)) {}
-  string_view(const char* data, size_t size) : m_data(data), m_size(size) {}
-
-  const char* data() const { return m_data; }
-  size_t size() const { return m_size; }
-
-  const char* m_data;
-  size_t m_size;
-};
-
-bool operator==(const string_view& a, const string_view& b) {
-  if (a.size() != b.size()) return false;
-  return memcmp(a.data(), b.data(), b.size()) == 0;
-}
-
-std::ostream& operator<<(std::ostream& os, const string_view& sv) {
-  if (sv.size()) os.write(sv.data(), static_cast<ssize_t>(sv.size()));
-  return os;
-}
 
 // Quick-and-dirty arena-allocated trie.
 
@@ -161,7 +138,7 @@ const char* tokenTypeName(TokenType type) {
 
 struct Token {
   TokenType type;
-  string_view text;
+  std::string_view text;
 };
 
 struct LexerResult {
@@ -502,7 +479,7 @@ struct Node {
 
   NodeType type = NodeType::Error;
   std::vector<Ptr<Node>> children;
-  string_view source;
+  std::string_view source;
 };
 
 } // namespace ast
@@ -526,7 +503,7 @@ struct Precedence {
   bool repeat;
 };
 
-Symbol key(const string_view& symbol) {
+Symbol key(const std::string_view& symbol) {
   Symbol key = 0;
   assert(symbol.size() <= sizeof(Symbol));
   memcpy(&key, symbol.data(), symbol.size());
@@ -606,7 +583,7 @@ size_t cursor(Env* env) {
     : env->input.size();
 }
 
-string_view source(Env* env, size_t pos, size_t end) {
+std::string_view source(Env* env, size_t pos, size_t end) {
   return {env->input.data() + pos, end - pos};
 }
 
